4f5a7d7f907fad60fef152c22c5fb3b912dc7aa5
Nov <78801499+N-ov@users.noreply.github.com>
2026-05-04 14:44:13 +0300

    Refactor methods in FFullScreenPassSceneViewExtension
    Refactor FFullScreenPassSceneViewExtension methods for clarity.

--- Source/FullScreenPass/Public/FullScreenPassSceneViewExtension.h
+++ Source/FullScreenPass/Public/FullScreenPassSceneViewExtension.h
@@ -5,12 +5,11 @@
 class FFullScreenPassSceneViewExtension : public FSceneViewExtensionBase
 {
 public:
-	FFullScreenPassSceneViewExtension(const FAutoRegister& AutoRegister);
+    FFullScreenPassSceneViewExtension(const FAutoRegister& AutoRegister);
 
-	//~ Begin FSceneViewExtensionBase Interface
-	virtual void SetupViewFamily(FSceneViewFamily& InViewFamily) override {};
-	virtual void SetupView(FSceneViewFamily& InViewFamily, FSceneView& InView) override {};
-	virtual void BeginRenderViewFamily(FSceneViewFamily& InViewFamily) override {};
-	virtual void PrePostProcessPass_RenderThread(FRDGBuilder& GraphBuilder, const FSceneView& View, const FPostProcessingInputs& Inputs) override;
-	//~ End FSceneViewExtensionBase Interface
+    virtual void PrePostProcessPass_RenderThread(
+        FRDGBuilder& GraphBuilder,
+        const FSceneView& View,
+        const FPostProcessingInputs& Inputs
+    ) override;
 };
