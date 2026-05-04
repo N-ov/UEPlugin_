f16df50538797788fba913826302d9964fb9916f
Nov <78801499+N-ov@users.noreply.github.com>
2026-05-04 14:43:38 +0300

    Refactor FullScreenPass shaders and parameters

--- Source/FullScreenPass/Public/FullScreenPassShaders.h
+++ Source/FullScreenPass/Public/FullScreenPassShaders.h
@@ -1,27 +1,44 @@
 #pragma once
+
 #include "CoreMinimal.h"
-#include "ScreenPass.h"
-#include "SceneTexturesConfig.h"
-
-
-class FFullScreenPassVS : public FGlobalShader
-{
-public:
-	DECLARE_GLOBAL_SHADER(FFullScreenPassVS);
-
-	FFullScreenPassVS() = default;
-	FFullScreenPassVS(const ShaderMetaType::CompiledShaderInitializerType& Initializer) : FGlobalShader(Initializer) {}
-};
+#include "GlobalShader.h"
+#include "RenderGraphResources.h"
+#include "ShaderParameterStruct.h"
 
 class FFullScreenPassPS : public FGlobalShader
 {
-	DECLARE_GLOBAL_SHADER(FFullScreenPassPS);
-	SHADER_USE_PARAMETER_STRUCT(FFullScreenPassPS, FGlobalShader);
+    DECLARE_GLOBAL_SHADER(FFullScreenPassPS);
+    SHADER_USE_PARAMETER_STRUCT(FFullScreenPassPS, FGlobalShader);
 
-	BEGIN_SHADER_PARAMETER_STRUCT(FParameters, )
-		RENDER_TARGET_BINDING_SLOTS()
-		SHADER_PARAMETER_STRUCT_REF(FViewUniformShaderParameters, View)
-		SHADER_PARAMETER_RDG_UNIFORM_BUFFER(FSceneTextureUniformParameters, SceneTexturesStruct)
-		SHADER_PARAMETER(float, Strenght)
-	END_SHADER_PARAMETER_STRUCT()
+public:
+    BEGIN_SHADER_PARAMETER_STRUCT(FParameters, )
+        SHADER_PARAMETER_STRUCT_REF(FViewUniformShaderParameters, View)
+
+        SHADER_PARAMETER_RDG_TEXTURE(Texture2D, InputTexture)
+        SHADER_PARAMETER_SAMPLER(SamplerState, InputSampler)
+
+        SHADER_PARAMETER_RDG_TEXTURE(Texture2D, BlurredTexture)
+        SHADER_PARAMETER_SAMPLER(SamplerState, BlurredSampler)
+
+        SHADER_PARAMETER_RDG_TEXTURE(Texture2D, SceneDepthTexture)
+        SHADER_PARAMETER_SAMPLER(SamplerState, DepthSampler)
+
+        SHADER_PARAMETER(FVector2f, InvBufferSize)
+
+        SHADER_PARAMETER(float, EffectStrength)
+        SHADER_PARAMETER(float, BlurStart)
+        SHADER_PARAMETER(float, FogStart)
+        SHADER_PARAMETER(float, FogFactor)
+        SHADER_PARAMETER(FVector3f, FogColor)
+
+        SHADER_PARAMETER(int32, BlurRadius)
+        SHADER_PARAMETER(int32, PassIndex)
+
+        RENDER_TARGET_BINDING_SLOTS()
+    END_SHADER_PARAMETER_STRUCT()
+
+    static bool ShouldCompilePermutation(const FGlobalShaderPermutationParameters& Parameters)
+    {
+        return IsFeatureLevelSupported(Parameters.Platform, ERHIFeatureLevel::SM5);
+    }
 };
